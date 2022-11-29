6c9ca69d1a176e1aff5d3223f77642b7b264af15
miticandrii <115782421+miticandrii@users.noreply.github.com>
2022-11-29 11:03:24 +0100

    Autetifikacija korisnika, register, login i logout

--- api.php
+++ api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\DomZdravljaController;
 use App\Http\Controllers\API\LekarController;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,12 @@ use App\Http\Controllers\API\LekarController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware' => ['auth:sanctum']], function () {
 Route::resource('dom_zdravlja',DomZdravljaController::class);
 Route::resource('lekar', LekarController::class);
+Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
